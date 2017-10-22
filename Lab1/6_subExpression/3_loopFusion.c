4f0604eca1e1d5291521690f4cbcf97fc8f8bd5f
trakaros <caxelos@inf.uth.gr>
2017-10-23 02:18:14 +0300

    strength reduction left

--- Lab1/6_subExpression/3_loopFusion.c
+++ Lab1/6_subExpression/3_loopFusion.c
@@ -38,17 +38,29 @@ unsigned char input[SIZE*SIZE], output[SIZE*SIZE], golden[SIZE*SIZE];
  * value is the convolution of the operator with the neighboring pixels of the*
  * pixel we process.														  */
 int convolution2D(int posy, int posx, const unsigned char *input, char operator[][3]) {
-	int i, j, res;
-  
+	int res;
+  	int value1, value2,value3;
+
+	value1 = (posy -1)<<12;
+	value2 = (posy)<<12;
+	value3 = (posy +1)<<12;
+
 	res = 0;
+	res += input[value1+ posx -1] * operator[0][0];
+	res += input[value1 + posx ] * operator[0][1];
+	res += input[value1 + posx + 1] * operator[0][2];
+
+	res += input[value2 + posx -1] * operator[1][0];
+	res += input[value2 + posx ] * operator[1][1];
+	res += input[value2 + posx + 1] * operator[1][2];
+
+	res += input[value3 + posx -1] * operator[2][0];
+	res += input[value3 + posx ] * operator[2][1];
+	res += input[value3 + posx + 1] * operator[2][2];
 	
 
-	for (i = -1; i <= 1; i++) {
-		for (j = -1; j <= 1; j++) {
-			res += input[(posy + i)*SIZE + posx + j] * operator[i+1][j+1];
-		}
-	}
-	return(res);
+	return res;
+
 }
 
 
@@ -106,36 +118,64 @@ double sobel(unsigned char *input, unsigned char *output, unsigned char *golden)
 	/* This is the main computation. Get the starting time. */
 	clock_gettime(CLOCK_MONOTONIC_RAW, &tv1);
 	/* For each pixel of the output image */
+	
 	for (i=1; i<SIZE-1; i+=1) {
-		for (j=1; j<SIZE-1; j+=1 ) {
-			/* Apply the sobel filter and calculate the magnitude *
-			 * of the derivative.								  */
+		for (j=1; j<SIZE-1; j+=4) {
+			//LOOP 1
 			p = pow(convolution2D(i, j, input, horiz_operator), 2) + 
 				pow(convolution2D(i, j, input, vert_operator), 2);
 			res = (int)sqrt(p);
-			/* If the resulting value is greater than 255, clip it *
-			 * to 255.											   */
 			if (res > 255)
 				output[i*SIZE + j] = 255;      
 			else
 				output[i*SIZE + j] = (unsigned char)res;
+	
+
+			//LOOP 2		 								  
+			p = pow(convolution2D(i, j+1, input, horiz_operator), 2) + 
+				pow(convolution2D(i, j+1, input, vert_operator), 2);
+			res = (int)sqrt(p);											  
+			if (res > 255)
+				output[i*SIZE + j+1] = 255;      
+			else
+				output[i*SIZE + j+1] = (unsigned char)res;
+
+
+			//LOOP 3					  
+			p = pow(convolution2D(i, j+2, input, horiz_operator), 2) + 
+				pow(convolution2D(i, j+2, input, vert_operator), 2);
+			res = (int)sqrt(p);											   
+			if (res > 255)
+				output[i*SIZE + j+2] = 255;      
+			else
+				output[i*SIZE + j+2] = (unsigned char)res;
+
+			
+		
+			//LOOP 4		 
+			p = pow(convolution2D(i, j+3, input, horiz_operator), 2) + 
+				pow(convolution2D(i, j+3, input, vert_operator), 2);
+			res = (int)sqrt(p);
+			if (res > 255)											  
+				output[i*SIZE + j+3] = 255;      
+			else
+				output[i*SIZE + j+3] = (unsigned char)res;					
+			
+		
+		}
+
+		for (j=1; j<SIZE-1; j++) {
+			t = pow((output[i*SIZE+j] - golden[i*SIZE+j]),2);	
+			PSNR += t;					
 		}
 	}
 
-	/* Now run through the output and the golden output to calculate *
-	 * the MSE and then the PSNR.									 */
-	for ( i=1; i<SIZE-1; i++) {
-		for (j=1; j<SIZE-1; j++) {
-			t = pow((output[i*SIZE+j] - golden[i*SIZE+j]),2);
-			PSNR += t;
-		}
-	}
-  
+
 	PSNR /= (double)(SIZE*SIZE);
 	PSNR = 10*log10(65536/PSNR);
 
-	/* This is the end of the main computation. Take the end time,  *
-	 * calculate the duration of the computation and report it. 	*/
+	// This is the end of the main computation. Take the end time,  
+	//calculate the duration of the computation and report it. 	
 	clock_gettime(CLOCK_MONOTONIC_RAW, &tv2);
 
 	printf ("Total time = %10g seconds\n",
@@ -143,7 +183,7 @@ double sobel(unsigned char *input, unsigned char *output, unsigned char *golden)
 			(double) (tv2.tv_sec - tv1.tv_sec));
 
   
-	/* Write the output file */
+	// Write the output file 
 	fwrite(output, sizeof(unsigned char), SIZE*SIZE, f_out);
 	fclose(f_out);
   
